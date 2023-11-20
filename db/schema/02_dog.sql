cb81f463891a6e6c632512ac521989e44f242861
Hamza Kaderi <kaderihamza4@gmail.com>
2023-11-20 14:11:43 -0800

    updated branch

--- db/schema/02_dog.sql
+++ db/schema/02_dog.sql
@@ -2,11 +2,11 @@ DROP TABLE IF EXISTS dog CASCADE;
 
 CREATE TABLE dogs (
     id SERIAL PRIMARY KEY NOT NULL,
+    user_id INT REFERENCES users(id) ON DELETE CASCADE,
     name VARCHAR(255) NOT NULL,
     age INT NOT NULL,
     breed VARCHAR(255) NOT NULL,
     adoption_fee INT NOT NULL,
-    admin_id INT REFERENCES admins(id) ON DELETE CASCADE,
     description TEXT,
     photo_url VARCHAR(255) NOT NULL
 );
--- db/schema/03_listing.sql
+++ db/schema/03_listing.sql
@@ -2,8 +2,13 @@ DROP TABLE IF EXISTS listing CASCADE;
 
 CREATE TABLE listings (
     id SERIAL PRIMARY KEY NOT NULL,
+    dog_id INT REFERENCES dogs(id) ON DELETE CASCADE
     publish_date DATE NOT NULL,
     adoptable BOOLEAN NOT NULL DEFAULT FALSE,
-    admin_id INT REFERENCES admins(id) ON DELETE CASCADE,
-    dog_id INT REFERENCES dogs(id) ON DELETE CASCADE
 );
+
+
+
+
+
+
