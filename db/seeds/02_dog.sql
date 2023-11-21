abe79d8df777e9ac1924c07873310e1c2e89dbab
Hamza Kaderi <kaderihamza4@gmail.com>
2023-11-20 19:58:11 -0800

    staging edits

--- db/seeds/02_dog.sql
+++ db/seeds/02_dog.sql
@@ -1,4 +1,4 @@
-INSERT INTO dogs (name, age, breed, adoption_fee, admin_id, description, photo_url)
+INSERT INTO dogs (name, age, breed, adoption_fee, user_id, description, photo_url)
 VALUES 
 ('Buddy', 3, 'Golden Retriever', 200, 1, 'Friendly and playful dog', 'https://example.com/buddy.jpg'),
 ('Max', 2, 'German Shepherd', 250, 2, 'Active and loyal companion', 'https://example.com/max.jpg'),
