bda4eb42a6978e7a88cfd4f65cee892554543d14
Rachel Little <racheldawnlittle@gmail.com>
2023-11-21 01:22:09 -0700

    updated listings to populate photo, added dogseeds

--- db/queries/listings.js
+++ db/queries/listings.js
@@ -2,7 +2,7 @@ const db = require('../connection');
 
 const getListings = () => {
   return db.query(`
-  SELECT listings.*, dogs.name, dogs.age, dogs.breed 
+  SELECT listings.*, dogs.name, dogs.age, dogs.breed, dogs.photo_url
   FROM listings
   JOIN dogs ON dog_id = dogs.id
   ;`)
