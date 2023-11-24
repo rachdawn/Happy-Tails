d96c6554fa28a339e4f35c6a53f51581805afad2
Rachel Little <racheldawnlittle@gmail.com>
2023-11-24 02:53:08 -0700

    style fixes

--- db/seeds/01_users.sql
+++ db/seeds/01_users.sql
@@ -1,6 +1,6 @@
 -- Users table seeds here (Example)
-INSERT INTO users (email, password, username) 
-VALUES 
-('alice@example.com', 'alice123', 'AliceUser'),
-('kira@example.com', 'kira456', 'KiraUser'),
-('bob@example.com', 'bob789', 'BobUser');
+INSERT INTO users (email, password, username, is_admin)
+VALUES
+('alice@example.com', 'alice123', 'AliceUser', true),
+('kira@example.com', 'kira456', 'KiraUser', true),
+('bob@example.com', 'bob789', 'BobUser', true);
