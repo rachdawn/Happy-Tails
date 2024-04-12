3b1da8b1a1642ff3faa1dd4b1d2615cfcfbce815
Rachel Little <racheldawnlittle@gmail.com>
2024-04-12 15:49:03 -0600

    update seeds

--- db/seeds/02_dog.sql
+++ db/seeds/02_dog.sql
@@ -6,7 +6,7 @@ VALUES
 ('Finn', 6, 'Chihuahua', 250, 2, 'Sweet and gentle temperament', 'https://images.saymedia-content.com/.image/c_limit%2Ccs_srgb%2Cq_auto:eco%2Cw_700/MjAxMjM1NTc0NjYzNjg1NjU0/chihuahua-guide.webp', true, '2023-11-10'),
 ('Josie', 1, 'Husky', 400, 1, 'Active and loyal companion', 'https://images.wagwalkingweb.com/media/daily_wag/blog_articles/hero/1685787498.877709/fun-facts-about-siberian-huskies-1.png', FALSE, '2023-11-16'),
 ('Lola', 10, 'Husky', 300, 2, 'Does not play well with others', 'https://waggys.pet/cdn/shop/articles/Waggys_Blog_16_Tipos_Husky.png?v=1683087710', true, '2023-11-22'),
-('Jax', 7, 'Basenji', 500, 3, 'Very energetic, needs a lot of exercise', 'https://www.yourpurebredpuppy.com/dogbreeds/photos-AB/basenjisf3.jpg', true, '2023-11-23'),
+('Jax', 7, 'Basenji', 500, 3, 'Very energetic, needs a lot of exercise', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSU-ic1-V4h5Uo1B5zvixVv43unM4TG5usFo9LHNvA5Cox2uWRU', true, '2023-11-23'),
 ('Hugo', 5, 'English Mastiff', 450, 3, 'Very lazy and sleepy, eats a lot', 'https://www.loveyourdog.com/wp-content/uploads/2020/05/Giant-English-Mastiff-in-Snow-900x500.jpg.webp', true, '2023-11-23'),
 ('Bubba', 8, 'Chihuahua', 350, 1, 'Loves to play', 'https://www.purina.co.uk/sites/default/files/styles/square_medium_440x440/public/2022-07/Chihuahua%20%28Long%20Coat%291.jpg?h=08f6fe0e&itok=ebyGLZCb', true, '2023-11-23'),
 ('Toby', 3, 'Chihuahua', 350, 1, 'Loves to play', 'https://cdn.britannica.com/44/233244-050-A65D4571/Chihuahua-dog.jpg', true, '2023-11-23');
