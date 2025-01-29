eab01d767372206014ce1de50049f6d422d83ed4
Rachel Little <racheldawnlittle@gmail.com>
2025-01-29 16:38:58 -0700

    test deployment

--- test-server.js
+++ test-server.js
@@ -2,9 +2,10 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
- app.get('/', (req, res) => {
-   res.send('Hello from test server!');
- });
+app.get('/', (req, res) => {
+  console.log("Request received at /"); // Add logging
+  res.send('Hello from test server!');
+});
 
  app.listen(PORT, () => {
    console.log(`Test server listening on port ${PORT}`);
