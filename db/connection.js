2d2aa5db9757c95618d64aae415db4b4af3652a4
Rachel Little <racheldawnlittle@gmail.com>
2025-01-28 21:30:47 -0700

    update db connection

--- db/connection.js
+++ db/connection.js
@@ -1,16 +1,20 @@
 // PG database client/connection setup
 const { Pool } = require('pg');
 
-const dbParams = {
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  database: process.env.DB_NAME
-};
+const db = new Pool({
+  connectionString: process.env.DATABASE_URL,
+});
+
+// const dbParams = {
+//   host: process.env.DB_HOST,
+//   port: process.env.DB_PORT,
+//   user: process.env.DB_USER,
+//   password: process.env.DB_PASS,
+//   database: process.env.DB_NAME
+// };
 
 
-const db = new Pool(dbParams);
+// const db = new Pool(dbParams);
 // console.log("db params djhad", dbParams);
 
 try {
