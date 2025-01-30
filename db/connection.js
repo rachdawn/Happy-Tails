53a58cae65e31ea1df872628166dbd8534ba81cb
Rachel Little <racheldawnlittle@gmail.com>
2025-01-30 16:37:05 -0700

    testing ssl for deployment issue

--- db/connection.js
+++ db/connection.js
@@ -2,9 +2,9 @@ const { Pool } = require('pg');
 
 const db = new Pool({
   connectionString: process.env.DATABASE_URL,
-  // ssl: { // Important for Neon!
-  //   rejectUnauthorized: false, // For development. In production, get a proper cert.
-  // },
+  ssl: { // Important for Neon!
+    rejectUnauthorized: false, // For development. In production, get a proper cert.
+  },
 });
 
 // Optional: Test the connection (good practice)
